97d01bf057a52d7f9c54b918f060d268ad12c106
Matthew Michaud <matthew.michaud@alumni.ucalgary.ca>
2024-05-19 16:48:11 -0600

    Moved the symbol table into the utilities TU. Implemented scope resolution in the scanner. Although parsing is bottom-up, production actions relevant to scanning are executed sequentially in the order of the file as required.

--- src/compiler/utilities.hpp
+++ src/compiler/utilities.hpp
@@ -42,6 +42,7 @@ struct TUBuffer
 {
 	char* _buf;		// A pointer to a TU's source buffer.
 	size_t _size;	// The TU's size.
+	bool _failed;	// Indicates a semantic error was detected during scanning.
 };
 
 
@@ -53,3 +54,51 @@ struct TUBuffer
  * @param info The token to highlight from the line.
  */
 void HighlightError(std::ostream& os, TUBuffer& src, TokenInfo& info);
+
+
+/**
+ * @brief A data structure to store program symbols in a scoped hierarchy.
+ * 
+ * @tparam I Type of identifiers in the scoped hierarchy.
+ * @tparam D Type of the declarations in the scoped hierarchy.
+ */
+template<typename I, typename D>
+struct SymbolTable
+{
+protected:
+	// A stack of scopes that map identifiers to AST definitions.
+	std::forward_list<std::map<I, D*>> _stackMap;
+
+public:
+	/**
+	 * @brief Mark the beginning of a new scope.
+	 */
+	void Enter();
+
+	/**
+	 * @brief Mark the end of a new scope.
+	 */
+	void Exit();
+
+	/**
+	 * @brief Attempts to define a new symbol in the current scope.
+	 * 
+	 * @param name The symbol's identifier (name).
+	 * @param node The AST node referred to by the symbol.
+	 * @return AST node that defines the symbol. If no such symbol is defined,
+	 * nullptr is returned.
+	 */
+	D* Define(I name, D* node);
+
+	/**
+	 * @brief Returns a pointer to the AST node referred to by the specfied
+	 * symbol, if it exists in any scope. The most recently defined instance of
+	 * @param name The symbol's identifier (name).
+	 * the symbol will be returned.
+	 * 
+	 * @return The AST node that defines the symbol. If no such symbol is
+	 * defined, nullptr is returned. A returned pointer indicates a name
+	 * collision.
+	 */
+	D* Lookup(I name);
+};
